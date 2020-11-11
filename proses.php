f8359fdae24b18e9a77e9ad013a06aef0979b0da
Dafa24 <akbarpradipa@gmail.com>
2020-11-11 21:58:48 +0700

    Frst Commit

---
+++ proses.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Proses</title>
+</head>
+<body>
+	<h1>Hola!, <?= $_POST["nama"];?><br>
+		Alamat mu berada di  <?= $_POST["alamat"];  ?></h1>
+</body>
+</html>
